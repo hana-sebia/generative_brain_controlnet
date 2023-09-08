5d1304f034c2b053fe9daa81bd726730287ea3bd
hana-sebia <hana.sebia@outlook.fr>
2023-09-08 18:54:41 +0200

    data exploration and visualisation

--- src/python/preprocessing/create_datalist.py
+++ src/python/preprocessing/create_datalist.py
@@ -1,38 +1,49 @@
 """ Script to create train, validation and test data lists with paths to t1w and flair images. """
 from pathlib import Path
+import os
+import random
 
 import pandas as pd
 
 
-def create_datalist(sub_dirs):
+def create_datalist(files):
+
     data_list = []
-    for sub_dir in sub_dirs:
-        images_paths = sorted(list(sub_dir.glob("**/*T1w.png")))
-        for image_path in images_paths:
-            flair_path = image_path.parent / (image_path.name.replace("T1w", "FLAIR"))
-            data_list.append({"t1w": str(image_path), "flair": flair_path})
+    for image_path in files:
+        for i in range(30):
+            fus_path = Path(image_path).parent / "fUS" / image_path.replace("ULM.png",f'01_rest_slice-{i}_fUS.png')
+            data_list.append({"ULM": "ULM/" + str(image_path), "fUS": str(fus_path)})
 
     return pd.DataFrame(data_list)
 
 
 def main():
-    output_dir = Path("/project/outputs/ids/")
+    output_dir = Path("outputs/ids/")
     output_dir.mkdir(parents=True, exist_ok=True)
 
-    data_dir = Path("/data/")
-    sub_dirs = sorted([x for x in data_dir.iterdir() if x.is_dir()])
+    data_dir_ulm = Path("data/ULM")
+    
 
-    train_sub_dirs = sub_dirs[:35000]
-    val_sub_dirs = sub_dirs[35000:35500]
-    test_sub_dirs = sub_dirs[35500:]
+    # List all image files in the directory
+    image_files = [f for f in os.listdir(data_dir_ulm) if f.endswith(".png")]
 
-    data_df = create_datalist(train_sub_dirs)
+    # Shuffle the list of image files randomly
+    random.shuffle(image_files)
+
+
+    # Calculate the number of images for each split
+    num_train = 25
+    num_validate = 5
+    num_test = 5
+
+
+    data_df = create_datalist(image_files[0:num_train])
     data_df.to_csv(output_dir / "train.tsv", index=False, sep="\t")
 
-    data_df = create_datalist(val_sub_dirs)
+    data_df = create_datalist(image_files[num_train:num_train + num_validate])
     data_df.to_csv(output_dir / "validation.tsv", index=False, sep="\t")
 
-    data_df = create_datalist(test_sub_dirs)
+    data_df = create_datalist(image_files[num_train + num_validate:len(image_files)])
     data_df.to_csv(output_dir / "test.tsv", index=False, sep="\t")
 
 
